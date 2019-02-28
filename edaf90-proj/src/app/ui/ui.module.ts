a57c6d1799fe6b5265e27aac9dc3bbd5e8d3395f
dat15akl <dat15akl@student.lu.se>
2019-02-28 14:33:52 +0100

    add routing, navbar

---
+++ edaf90-proj/src/app/ui/ui.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { WeatherComponent } from './weather/weather.component';
+import { FilmComponent } from './film/film.component';
+import { HomeComponent } from './home/home.component';
+import { Home2Component } from './home2/home2.component';
+
+
+const appRoutes: Routes = [
+  { path: 'home', component: Home2Component },
+  { path: 'weather', component: WeatherComponent },
+  { path: 'film', component: FilmComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
+  ],
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent, WeatherComponent, FilmComponent, Home2Component],
+  exports: [LayoutComponent]
+})
+export class UiModule { }
