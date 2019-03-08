0dd42b05bec27cd78b1bd449e139bf250a81e447
Anders Klint <anders.klint519@gmail.com>
2019-03-08 17:01:14 +0100

    small little comment

--- edaf90-proj/src/app/app-routing.module.ts
+++ edaf90-proj/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// OBS: routes located in ui/ui.module.ts
 const routes: Routes = [];
 
 @NgModule({
